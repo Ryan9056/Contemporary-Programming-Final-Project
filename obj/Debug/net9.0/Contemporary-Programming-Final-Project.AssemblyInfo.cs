501841d8bee22f2436c39ab7a1a785858a734c72
Corey DeRossett <155482837+derossca@users.noreply.github.com>
2025-11-12 19:23:54 -0500

    restoring gitattributes and gitignore that ryan had added

--- obj/Debug/net9.0/Contemporary-Programming-Final-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Contemporary-Programming-Final-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contemporary-Programming-Final-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21408695d33ed1ac98e4ebd70fbb2914835c34a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c8a58b25b687a82ee544fd0575eec071e39bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contemporary-Programming-Final-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contemporary-Programming-Final-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
