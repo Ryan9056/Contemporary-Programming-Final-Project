907930195bd8c63d30d0ec5c2095a66f50b9c9b7
Corey DeRossett <155482837+derossca@users.noreply.github.com>
2025-11-17 20:42:30 -0500

    refactored program class, all controllers functioning properly

--- Program.cs
+++ Program.cs
@@ -26,6 +26,7 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
     Seeder.Seed(context);
 }
 
